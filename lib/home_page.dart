a685e7fe0922115fbfce0e60f7607d668fcbcc4d
im-sg99 <sg5260@naver.com>
2022-05-18 22:35:56 +0900

    Feat:Floating action button

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -8,6 +10,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  var mainColor = const Color(0xff6524FF);
+  var buttonSize = const Size(80,80);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,28 +21,55 @@ class _HomePageState extends State<HomePage> {
         child: Column(
           children: const [
             Text('Kant',
-              style: TextStyle(fontWeight: FontWeight.bold,
-              fontSize:48,
-              fontFamily: "Roboto",
-              color: Color(0xff6524FF),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize:48,
+                letterSpacing: -2,
+                fontFamily: "Roboto",
+                color: Color(0xff6524FF),
               ),
             ),
+            SizedBox(
+              height:70,
+            ),
+            Text("아아아아"),
           ],
         ),
       ),
       floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 30,right: 30),
-        child: SizedBox(
-          height: 70,
-          width: 70,
-          child: FloatingActionButton(
-            child: const Icon(
-              Icons.add,
-              size: 35,
+        padding: const EdgeInsets.only(bottom: 20,right: 20),
+        child: SpeedDial(
+          icon: Icons.add,
+          activeIcon: Icons.close,
+          spaceBetweenChildren: 30,
+          backgroundColor: mainColor,
+          overlayColor: Colors.black,
+          overlayOpacity: 0.6,
+          buttonSize: buttonSize,
+          // childPadding: const EdgeInsets.only(right: 20),
+          childrenButtonSize: buttonSize,
+          children: [
+            SpeedDialChild(
+              child: const Icon(
+                Icons.alarm_add,
+                size: 30,
+                color: Colors.white,
+              ),
+              onTap: () {},
+              backgroundColor: mainColor,
+              label: '알람',
             ),
-            onPressed: (){},
-            backgroundColor: const Color(0xff6524FF),
-          ),
+            SpeedDialChild(
+              child:const Icon(
+                Icons.alarm_sharp,
+                size: 30,
+                color: Colors.white,
+              ),
+              onTap: () {},
+              backgroundColor: mainColor,
+              label: '퀵설정',
+            )
+          ],
         ),
       ),
     );
